0ec1981b078f4412967e3ce6a7ee0ada4b8b9938
Pipas_ <19pipas@gmail.com>
2018-03-18 12:56:53 +0000

    Imported files and basic movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+	private Tile GetTile(Vector3 direction)
+    {
+        RaycastHit hit;
+        Tile tile = null;
+
+        if (Physics.Raycast(transform.position, direction, out hit, 1))
+        {
+            tile = hit.collider.GetComponent<Tile>();
+        }
+
+        return tile;
+    }
+
+	public void Move(Vector3 direction)
+	{
+		if(GetTile(direction) == null)
+		{
+			transform.Translate(direction);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Movement 
+{	
+	private Rigidbody playerRB;
+	void Start ()
+	{
+		playerRB = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		if(playerRB.velocity == Vector3.zero)
+		{
+			if(Input.GetKeyDown(KeyCode.W))
+				Move(Vector3.forward);
+			else if(Input.GetKeyDown(KeyCode.S))
+				Move(Vector3.back);
+			else if(Input.GetKeyDown(KeyCode.A))
+				Move(Vector3.left);
+			else if(Input.GetKeyDown(KeyCode.D))
+				Move(Vector3.right);
+		}
+
+		if(transform.position.y < -10)
+		{
+			ResetPlayer();
+		}
+	}
+
+	void ResetPlayer()
+	{
+		playerRB.velocity = Vector3.zero;
+		transform.position = new Vector3(0f, 0.9f, -1f);
+	}
+}
