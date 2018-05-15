fcc66d3e020491550ac6282c473fa5d90cdd3956
Claudia Marinho <claudia.m.r.marinho@gmail.com>
2018-05-15 05:53:22 +0100

    Added health bar and one enemy
    To do:
    - animate projectile of attack
    - make enemy play sounds
    - change enemy animation when attacking

---
+++ Assets/Scripts/Player Party/AscendingObjectCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AscendingObjectCollider : MonoBehaviour 
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        transform.parent.GetComponent<AscendingObject>().AddObject(other);
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        transform.parent.GetComponent<AscendingObject>().RemoveObject(other);
+    }
+}
