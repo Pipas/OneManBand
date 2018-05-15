49957b7e564f586f9e0fcf487e2befd2fec56d1b
Claudia Marinho <claudia.m.r.marinho@gmail.com>
2018-05-15 15:36:57 +0100

    Enemy walks back and forth
    To do:
    - enemy attack projectile (important)
    - stop enemy when facing player (important)
    - add sounds to attacks

---
+++ Assets/Scripts/Enemies/EnemyHitbox.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitbox : MonoBehaviour {
+    private bool playerWithinRange = false;
+    private bool onCooldown = false;
+    private float attackCooldown = 3.0f;
+
+    public HealthSystem playerHealth;
+	
+	// Update is called once per frame
+    void Update()
+    {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+            if (hit.collider.name == "Player")
+            {
+                playerWithinRange = true;
+            }
+            else
+            {
+                playerWithinRange = false;
+            }
+        }
+        else
+        {
+            playerWithinRange = false;
+        }
+
+        if (onCooldown)
+        {
+            if (attackCooldown <= 0)
+            {
+                attackCooldown = 3.0f;
+                onCooldown = false;
+            }
+            else
+            {
+                attackCooldown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (playerWithinRange) {
+                playerHealth.TakeDamage(-1);
+                onCooldown = true;
+            }
+        }
+    }
+
+    public bool isPlayerWithinRange()
+    {
+        return playerWithinRange;
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public Transform[] points;
+    private float speed = 1.5f;
+    private int currentPoint = 0;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Animator>().SetBool("RunToggle", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentPoint < points.Length) // this is to check if it's less than the length of the points
+        {
+            float dist = Vector3.Distance(transform.position, points[currentPoint].position);
+
+            if (dist < 0.1f)
+            {
+                currentPoint++;
+            }
+            else
+            {
+                transform.LookAt(points[currentPoint].position);
+                transform.position = Vector3.Lerp(this.transform.position, points[currentPoint].position, Time.deltaTime * speed);
+            }
+        }
+        else
+        {
+            currentPoint = 0; //this is to loop it back to zero again
+        }
+	}
+
+
+}
