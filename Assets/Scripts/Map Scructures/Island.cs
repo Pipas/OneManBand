b93fbbf9a173ba5a81198dd511c32ad8c62469f0
Claudia Marinho <claudia.m.r.marinho@gmail.com>
2018-06-14 01:19:33 +0100

    Import guitar to scene, still need to add collider
    and the rest xD

--- Assets/Scripts/Map Scructures/Island.cs
+++ Assets/Scripts/Map Scructures/Island.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Island : MonoBehaviour 
 {
-    public int cameraShift = 12;
+    public float cameraShift = 10.5f;
     // Use this for initialization
     void Start () {
         
