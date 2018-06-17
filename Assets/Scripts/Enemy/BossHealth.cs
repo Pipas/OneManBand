049ca0c646dd4da3251e386f75ac336d53c51ab4
Claudia Marinho <claudia.m.r.marinho@gmail.com>
2018-06-17 21:43:58 +0100

    nao sei se funciona

---
+++ Assets/Scripts/Enemy/BossHealth.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour {
+    public GameObject enemy;
+    public KillEnemy killEnemy;
+    
+    public int startHearts = 3;
+    public int currHealth;
+
+    private int healthPerHeart = 1;
+
+    public Image[] healthImages;
+    public Sprite[] healthSprites;
+
+    void Start() {
+        Skillbar.ActivateSkill("PartyTambor");      
+
+        currHealth = startHearts * healthPerHeart;
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        bool empty = false;
+        int i = 0;
+
+        foreach (Image image in healthImages)
+        {
+            if (empty)
+            {
+                image.sprite = healthSprites[0];
+            }
+            else
+            {
+                i++;
+                if (currHealth >= i * healthPerHeart)
+                {
+                    image.sprite = healthSprites[healthSprites.Length - 1];
+                }
+                else
+                {
+                    int currentHeartHealth = (int)(healthPerHeart - (healthPerHeart * i - currHealth));
+                    int healthPerImage = healthPerHeart / (healthSprites.Length - 1);
+                    int imageIndex = currentHeartHealth / healthPerImage;
+
+                    image.sprite = healthSprites[imageIndex];
+                    image.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
+                    empty = true;
+                }
+            }
+        }
+
+        checkIfBossKilled();
+    }
+
+    public void checkIfBossKilled()
+    {
+        if (currHealth == 0)
+        {
+            killEnemy.setEnemyDead(true);
+            Destroy(enemy);
+        }
+    }
+    public void TakeDamage(int amount)
+    {
+        currHealth += amount;
+        currHealth = Mathf.Clamp(currHealth, 0, startHearts * healthPerHeart);
+        UpdateHearts();
+    }
+
+}
--- Assets/Scripts/Enemy/KillEnemy.cs
+++ Assets/Scripts/Enemy/KillEnemy.cs
@@ -4,24 +4,7 @@ using UnityEngine;
 
 public class KillEnemy : MonoBehaviour {
     public GameObject enemy;
-
     private bool enemyDead = false;
-    /*private Color alphaColor;
-    private float timeToFade = 1.0f;*/
-
-	// Use this for initialization
-	void Start () {
-        /*alphaColor = enemy.GetComponent<MeshRenderer>().material.color;
-        alphaColor.a = 0;*/
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        /*if (enemyDead)
-        {
-            enemy.GetComponent<MeshRenderer>().material.color = Color.Lerp(enemy.GetComponent<MeshRenderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
-        }*/
-	}
 
     public bool isEnemyDead()
     {
